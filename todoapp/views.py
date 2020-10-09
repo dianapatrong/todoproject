71ce07de3dce5c9963a625f711b42bbe0d0bcfc3
Diana Patron <patrondiana13@hotmail.com>
2020-10-09 14:56:56 -0500

    Moved todoApp route to root

--- todoapp/views.py
+++ todoapp/views.py
@@ -12,11 +12,11 @@ def add_todo_view(request):
     todo_content = request.POST['content']
     new_todo_item = TodoListItem(content=todo_content)
     new_todo_item.save()
-    return HttpResponseRedirect('/todoapp/')
+    return HttpResponseRedirect('/')
 
 
 def delete_todo_view(request, i):
     print("REQUEST: ", request, i)
     item = TodoListItem.objects.get(id=i)
     item.delete()
-    return HttpResponseRedirect('/todoapp/')
+    return HttpResponseRedirect('/')
--- todoproject/urls.py
+++ todoproject/urls.py
@@ -19,7 +19,7 @@ from todoapp.views import todo_app_view, add_todo_view, delete_todo_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todoapp/', todo_app_view),
+    path('', todo_app_view),
     path('addTodoItem/', add_todo_view),
     path('deleteTodoItem/<int:i>/', delete_todo_view)
 ]
