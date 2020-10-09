88b3429ce207c73f20b0aeda5f0abfba02843625
Diana Patron <patrondiana13@hotmail.com>
2020-10-09 16:34:05 -0500

    Added placeholder when submitting new item and style

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -3,6 +3,8 @@ from .models import TodoListItem
 
 
 class TodoListItemForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new todo item...'}))
+
     class Meta:
         model = TodoListItem
         fields = '__all__'
