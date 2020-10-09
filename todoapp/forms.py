b95b863cda2e9b79813f048623d621deded70f4a
Diana Patron <patrondiana13@hotmail.com>
2020-10-09 17:27:04 -0500

    Styled update and delete pages

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -3,8 +3,6 @@ from .models import TodoListItem
 
 
 class TodoListItemForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new todo item...'}))
-
     class Meta:
         model = TodoListItem
         fields = '__all__'
