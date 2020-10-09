90fa6b137475b7be95290b8c8ee554f47dfc37a8
Diana Patron <patrondiana13@hotmail.com>
2020-10-09 14:05:43 -0500

    Get all todo items,  return it to template and display it

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import TodoListItem
 
 
 def todo_app_view(request):
-    return render(request, 'todolist.html')
+    all_todo_items = TodoListItem.objects.all()
+    return render(request, 'todolist.html', {'all_items': all_todo_items})
