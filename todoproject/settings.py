92797e9b71abe2c329f42a3febab2949a2b1e76f
Diana Patron <patrondiana13@hotmail.com>
2020-10-10 14:38:42 -0500

    Exported user as env variable in travis and settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'todo_proj',
-        'USER': 'postgres',
+        'USER': os.environ['DB_USER'],
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
