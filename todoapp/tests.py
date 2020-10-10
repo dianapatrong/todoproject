cebd7aeee9fcdb682ca8bc569e0d234393a029ab
Diana Patron <patrondiana13@hotmail.com>
2020-10-10 01:57:57 -0500

    Refactored unit tests

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -10,14 +10,14 @@ class ViewsTestCase(TestCase):
         self.client = Client()
         self.list_url = reverse('list')
         self.add_item_url = reverse('add_item')
-        self.update_item_url = reverse('update_item', args=[2])
-        self.delete_item_url = reverse('delete', args=[1])
-        """self.item1 = TodoListItem.objects.create(
-            id=1,
+        self.update_item_url = reverse('update_item', args=[10])
+        self.delete_item_url = reverse('delete', args=[10])
+        self.item1 = TodoListItem.objects.create(
+            id=10,
             title='Buy milk',
             created=datetime.now(),
             complete=False
-        )"""
+        )
 
     def test_todo_view(self):
         response = self.client.get(self.list_url)
@@ -37,51 +37,27 @@ class ViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_update_item_GET(self):
-        TodoListItem.objects.create(
-            id=2,
-            title='Buy milk',
-            created=datetime.now(),
-            complete=False
-        )
         response = self.client.get(self.update_item_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'update_todolist.html')
 
     def test_update_item_POST(self):
-        TodoListItem.objects.create(
-            id=2,
-            title='Buy milk',
-            created=datetime.now(),
-            complete=False
-        )
         item = {
             'title': 'Buy milk and eggs',
-            'complete': 't'
+            'complete': False
         }
         response = self.client.post(self.update_item_url, item)
         self.assertEqual(response.status_code, 302)
-        item2 = TodoListItem.objects.get(id=2)
+        item2 = TodoListItem.objects.get(id=10)
         self.assertEqual(item2.title, 'Buy milk and eggs')
 
     def test_delete_item_GET(self):
-        TodoListItem.objects.create(
-            id=1,
-            title='Buy milk',
-            created=datetime.now(),
-            complete=False
-        )
         response = self.client.get(self.delete_item_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'delete.html')
 
     def test_delete_item_POST(self):
-        TodoListItem.objects.create(
-            id=1,
-            title='Buy milk',
-            created=datetime.now(),
-            complete=False
-        )
         response = self.client.post(self.delete_item_url)
         self.assertEqual(response.status_code, 302)
-        self.assertFalse(TodoListItem.objects.filter(id=2).exists())
+        self.assertFalse(TodoListItem.objects.filter(id=10).exists())
 
