352e5e03b14eade511fefe9eccbf285dcf403e81
Diana Patron <patrondiana13@hotmail.com>
2020-10-10 14:24:05 -0500

    Changed db to postgres

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'todoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todo_proj',
+        'USER': 'dpatron',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
