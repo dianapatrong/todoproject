5650aee659fe866e76e14c2601a36a4c3743c1e1
Diana Patron <patrondiana13@hotmail.com>
2020-10-10 14:34:51 -0500

    Changed db user

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'todo_proj',
-        'USER': 'dpatron',
+        'USER': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
