4a23a4369a90fded96543611350a8160d0fbe0c5
Diana Patron <patrondiana13@hotmail.com>
2020-10-14 12:55:27 -0500

    Changed Travis for github workflow (#6)
    * Added github actions workflow for tests
    
    * Added deployment with Github Actions
    
    * Delete travis file

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -79,11 +79,23 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'todo_proj',
-        'USER': os.environ['DB_USER'],
+        'USER': 'todouser',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
